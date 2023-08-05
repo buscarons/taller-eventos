1a55930d4acef5ef57a2fa067aa5bfeadf4a4ed8
Israel Buscarons <israelbuscarons@gmail.com>
2023-08-05 19:57:05 -0300

    Solucion a propagacion de evento

--- eventos.js
+++ eventos.js
@@ -1,4 +1,11 @@
 const contenedor = document.querySelector("div");
+const btn = document.querySelector("button");
+
+function alertaBoton(event) {
+    alert('Hola!');
+    event.stopPropagation();
+}
+
 
 contenedor.addEventListener("click", () => {
     alert('Hola soy el div!')
